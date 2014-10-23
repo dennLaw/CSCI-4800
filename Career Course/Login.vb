c40fb346e7e2dc4d3e62b40e4d80670000953ea0
Brian Connerton <brianc93@uga.edu>
2014-10-23 16:12:48 -0400

    Created login screen for student course

---
+++ Career Course/Login.vb
@@ -0,0 +1,14 @@
+﻿Public Class Login
+
+    Private Sub Login_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+
+    End Sub
+End Class
